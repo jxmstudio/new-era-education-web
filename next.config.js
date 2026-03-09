fe42e6e5816cff902c882a06d558a6de72df6d84
alexngai1 <163654935+alexngai1@users.noreply.github.com>
2026-03-09 12:47:24 +1100

    Install Meta Pixel and update CSP

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://www.google.com https://maps.google.com https://maps.googleapis.com https://*.google.com https://*.googleapis.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://maps.gstatic.com https://www.googletagmanager.com; connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://maps.googleapis.com https://maps.gstatic.com; object-src 'none';",
+            value: "frame-src 'self' https://www.google.com https://maps.google.com https://maps.googleapis.com https://*.google.com https://*.googleapis.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://maps.gstatic.com https://www.googletagmanager.com https://connect.facebook.net; connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://maps.googleapis.com https://maps.gstatic.com https://www.facebook.com https://connect.facebook.net; object-src 'none';",
           },
         ],
       },
